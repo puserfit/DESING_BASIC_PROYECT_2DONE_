c05e5ab9334dfbaf89e8b02d65fe17a9e845d4cb
caaledcarre <90487078+caaledcarre@users.noreply.github.com>
2023-07-19 21:33:47 -0500

    Modificaciones en la UI Pausa
    Se modifico la UI Pause haciendola totalmente Diegetica. Se quito el boton musica y se coloco en el mismo panel el boton de continuar y el boton de pausa quedo con la tecla "Escape"

--- Assets/Script/PauseController.cs
+++ Assets/Script/PauseController.cs
@@ -10,39 +10,68 @@ using UnityEngine.SceneManagement;
 //clase:PauseController  (componente de Unity) y Hereda:MonoBehaviour
 public class PauseController : MonoBehaviour
 {    
-    [SerializeField] GameObject pauseMenu;     //GameObject:pauseMenu  (representa el PANEL en la interfaz de usuario)
+    //[SerializeField] GameObject pauseMenu;     //GameObject:pauseMenu  (representa el PANEL en la interfaz de usuario)
+	[SerializeField] private GameObject pauseMenu; 
 	                                           //Atributo:SerializeField   (permite que este campo sea visible en Unity..para que se pueda
 											   //asisgnar el boton de pausa al arrastrar y soltar el objeto correspondiente en el campo)
 	
 	                                         // Asignar el panel PauseMenu a este objeto desde 
                                             // el elemento 'script' en el inspector, una vez
                                             // este script se haya asociado a los botones.
+											
+											
+											
+	//NUEVO CODIGO PARA ESTA ESCENA DE PAUSA
+	
+	private bool paused = false;    //Controla si el juego esta pausado o no. Cuando es "true" significa que el juego esta pausado.
+	
+	//La tecla ESC controla la pausa
+	void Update()    //metodo:Update
+	{   
+	    //Verifica si la tecla "Escape" --"KeyCode.Escape" se ha presionado.
+		if(Input.GetKeyDown(KeyCode.Escape)){     
+			if(paused){                      //si se ha presionado cambia entre "pausa--"Pause()" o "reanudar---"Resume()" segun el estado actual de "paused"
+				Resume();
+				
+			}
+			else{
+				Pause();
+			}
+		}
+	}										
     
+	//fin
 	
 	//BOTON PAUSA
     public void Pause(){                   //Metodo:Pause     GameObject:pauseMenu
-        Debug.Log("BUTTON -> Pause");      //este metodo se llama cuando el jugador presiona el boton de pausa
+        //Debug.Log("BUTTON -> Pause");      //este metodo se llama cuando el jugador presiona el boton de pausa
+		Debug.Log("PAUSE"); 
         pauseMenu.SetActive(true);          //se establece el GameObject:pauseMenu en activo "SetActive(true)"...el panel de pausa se mostrara en la ui.
         Time.timeScale = 0f;                //Time.timeScale = 0f Al tener esto. Pausamos el tiempo de juego.
+		paused = !paused;
     }
 
 	
 	//BOTON CONTINUAR
     public void Resume(){                   //Metodo:Resume  (se llamaba cuando el jugador presiona el boton de reanudar)
-        Debug.Log("BUTTON -> Resume");      // Debug que muestra la persona oprimio el boton.
+        //Debug.Log("BUTTON -> Resume");      // Debug que muestra la persona oprimio el boton.
+		Debug.Log("PLAY");
         pauseMenu.SetActive(false);         //se establece el GameObject:pauseMenu en inactivo "SetActive(false)"...el panel de pausa se oculatara en la ui.
         Time.timeScale = 1f;                //como esta en 1 reanuda el tiempo de juego.
+		paused = !paused;
     }
 
 	
 	//BOTON REGRESAR MENU
     public void goMenu(int _escena){        //medodo:goMenu(int _escena   (se llama cuando el jugador presiona el boton de ir al menu principal)
+	    //Debug.Log("BUTTON -> Home");
+	    Debug.Log("BUTTON -> Menu");
         Time.timeScale = 1f;                //Time.timeScale en 1 para asegurarse de que el tiempo de juego se reanude correctamente
         SceneManager.LoadScene(_escena);     //se carga la escena especificada por el parametro:_escena
     }
     
 	//BOTON AUDIO
-    public void Audio(){                     //metodo:Audio (se llama cuando el jugador presiona el boton de audio)
+    /*public void Audio(){                     //metodo:Audio (se llama cuando el jugador presiona el boton de audio)
         Debug.Log("BUTTON -> Audio");        //Imprime un mensaje de depuracion indicando que se ha presionado el boton de audio.
-    }
+    }*/
 }
--- Assets/Script/SceneController.cs
+++ Assets/Script/SceneController.cs
@@ -12,6 +12,28 @@ public class SceneController : MonoBehaviour  //clase:SceneController   hereda:M
 {
     public void changeScene(int _escena){           //metodo: "changeScene" permite cambiar la escena cargada actualmente     parametro:"_escena" numero entero
       Debug.Log("CALL -> changeScene: " + _escena);    //Mensaje de depuracion: "debug.Log"...indica que ha llamdo a este metodo.   parametro: _escena
+	  
+	  
+	  //se agrego mas codigo
+	  AudioSource audio = gameObject.GetComponent<AudioSource>(); //Si el objeto no tiene componente de audio esta variable será NULL
+      
+      if(audio == null){   // Si el objeto NO TIENE componente de AUDIO se cambia de escena inmediatamente
+         SceneManager.LoadScene(_escena);
+      }
+      else{                // Si el objeto SI TIENE componente de AUDIO solo se cambia de escena cuando el audio haya terminado de reproducirse
+         Debug.Log("[changeScene] AudioSource:  " + audio);
+         StartCoroutine(changeSceneWithAudio(_escena, audio));
+      }
+      Debug.Log("CALL -> changeScene: " + _escena + " END");
+   }
+
+   private IEnumerator changeSceneWithAudio(int _escena, AudioSource audio){
+      Debug.Log("[changeScene]Yield Init");
+      yield return new WaitUntil(() => audio.isPlaying == false); // Se ejecuta esta línea hasta que la condición sea verdadera
+      Debug.Log("[changeScene]Yield Over");
+	  //fin.
+	  
+	  
 	                                                   //changeScene: 1       si paso de splash (0) al menu(1)...y comienzo en la de splash
       SceneManager.LoadScene(_escena);               //"SceneManagement.LoadScene" cargar la escena correspondiente al parametro:"_escena"
    }
