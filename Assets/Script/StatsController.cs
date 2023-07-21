1fb6eb3d58678bd58d7b258e85b59615c50a467b
caaledcarre <90487078+caaledcarre@users.noreply.github.com>
2023-07-21 17:52:11 -0500

    Se descargo el paquete Pixel Aventure
    Se descargo el paquete Pixel Aventure y se cambio la imagen de los items en la UI pausa

--- Assets/Script/StatsController.cs
+++ Assets/Script/StatsController.cs
@@ -1,8 +1,8 @@
-using System.Collections;
+using System.Collections;           //usar colecciones como listas y diccionarios
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
+using UnityEngine;                  //Contiene las clases y funciones necesarias en Unity
+using UnityEngine.UI;               //Para trabajar con componentes de Interfaz de Usuario (UI) en Unity
+using TMPro;                        //Para trabajar con el paquete "TextMeshPro" para trabajar con textos
 
 
 //CONTROLADOR DE ESTADISTICAS
@@ -15,7 +15,8 @@ using TMPro;
 
 
 
-public class StatsController : MonoBehaviour
+public class StatsController : MonoBehaviour     //Clase:StatsController   Hereda:MonoBehaviour 
+                                                 //Permite que este codigo sea adjuntado a objetos en la escena y se ejecute en Unity    
 {
     private TMP_Text    _score     = null;    //Componente:"TMP_Text"  Referencia:"_score"  Muesta el puntaje del jugador 
 	
@@ -57,41 +58,58 @@ public class StatsController : MonoBehaviour
     }
 
 	
+	//Actualiza visualmente LAS VIDAS DEL JUGADOR
 	//Metodo:"updateLives((bool live1, bool live2, bool live3)lives)" Se inicializan los componentes 
-	
-    public void updateLives((bool live1, bool live2, bool live3)lives){
+	                                                                         //Tupla:secuencia de valores agrupados.  no puede ser modificada 
+																			 //EJM: tupla = (13, "mio", true) esta es una tupla triple
+																			 
+    public void updateLives((bool live1, bool live2, bool live3)lives){      //Recive como parametro una Tupla de 3 valores booleanos : "live1", "live2", "live3"
+	                                                                         //Estos parametros indican si cada vida esta activa o no (llena o vacia)
         _lives[0].enabled = lives.live1?true:false;
         _NO_lives[0].enabled = !_lives[0].enabled;
-        _lives[1].enabled = lives.live2?true:false;
-        _NO_lives[1].enabled = !_lives[1].enabled;
+        _lives[1].enabled = lives.live2?true:false;                          //a travez de "enabled" los componentes:"_lives" o "_NO_lives" 
+        _NO_lives[1].enabled = !_lives[1].enabled;                           //se muestra u oculta visualmente las vidas llenas o vacias segun el valor booleano.
         _lives[2].enabled = lives.live3?true:false;
         _NO_lives[2].enabled = !_lives[2].enabled;
     }
-
-    public void updateScore(int score){
-        if(score < 0){
-            _score.text = "00000";
+    
+	
+	//Actualiza visualmente EL PUNTAJE DEL JUGADOR
+	//Metodo:"updateScore(int score)"
+    public void updateScore(int score){         //Parametro: "int score"  este es el valor del puntaje el cual es un entero "int"
+	                                           
+											   
+        if(score < 0){                          //Si el puntaje es menonr que 0
+            _score.text = "00000";             //Se muestra "00000"
         }
-        else if(score > 99999){
-            _score.text = "99999";
+        else if(score > 99999){                 //Si el puntaje es mayor que 99999
+            _score.text = "99999";              //Se muestra "99999"
         }
-        else{
-            _score.text = score.ToString("00000");
+        else{                                        //Si el puntaje esta entre 0 y 99999.
+            _score.text = score.ToString("00000");   //Se muestra con ceros a la izquierda para completar un total de cinco digitos
         }
     }
 
-    public void updateItems((int item1, int item2, int item3)items){
-        if(items.item1 < 0){
-            _items[0].text = "00";
+	//Actualiza visualmente LA CANTIDAD DE ITEMS que tiene el jugador 	
+	//EJM: manzanas, peras, bananos ...que fueron recogidos por el jugador
+	//Metodo:updateItems((int item1, int item2, int item3)items)
+	
+    public void updateItems((int item1, int item2, int item3)items){   //Recive como parametro una tupla de 3 valores enteros "item1", "item2", "item3"
+	                                                                   //Representan la cantidad de cada tipo de item
+																	   
+		//PRIMER ITEM													   
+        if(items.item1 < 0){                                           //Si la cantidad de Item es menonr que 0                
+            _items[0].text = "00";                                     //Se muestra "00"
         }
-        else if(items.item1 > 99){
-            _items[0].text = "99";
+        else if(items.item1 > 99){                                     //Si la cantidad de Item es mayor que 99
+            _items[0].text = "99";                                     //Se muestra "99"
         }
         else{
-            _items[0].text = items.item1.ToString("00");
-        }
+            _items[0].text = items.item1.ToString("00");              //Si la cantidad de Item esta entre 0 y 99.
+        }                                                             //Se muestra con ceros a la izquierda para completar un total de dos digitos
 
-        if(items.item2 < 0){
+		//SEGUNDO ITEM
+        if(items.item2 < 0){     
             _items[1].text = "00";
         }
         else if(items.item2 > 99){
@@ -101,6 +119,7 @@ public class StatsController : MonoBehaviour
             _items[1].text = items.item2.ToString("00");
         }
 
+		//TERCER ITEM
         if(items.item3 < 0){
             _items[2].text = "00";
         }
@@ -112,16 +131,20 @@ public class StatsController : MonoBehaviour
         }
     }
 
-    public void updatePowers((bool item1, bool item2, bool item3)powers){
-        Color tempColor;
-
-        tempColor = _powers[0].color;
+	//Actualiza visualmente LOS PODERES DEL JUGADOR
+	//Metodo:"updatePowers((bool item1, bool item2, bool item3)powers)" 
+	
+    public void updatePowers((bool item1, bool item2, bool item3)powers){   //Recibe como parametro una tupla de 3 valores booleanos "item1", "item2", "item3"
+	                                                                        //Indican si cada poder esta activo o no
+        Color tempColor;                                                    //A travez de la propiedad "color" de los componentes "_powers"
+                                                                            //Se cambia el color de los poderes que estan:
+        tempColor = _powers[0].color;                                       
         tempColor.r = powers.item1?1f:0f;
         tempColor.g = powers.item1?1f:0f;
         tempColor.b = powers.item1?1f:0f;
-        tempColor.a = powers.item1?1f:0.2392f;
-        _powers[0].color = tempColor;
-
+        tempColor.a = powers.item1?1f:0.2392f;                              //Activo:Blanco    
+        _powers[0].color = tempColor;                                       //Desactivado: Opacidad mas baja 
+                                                                            //segun el valor del booleano.
         tempColor = _powers[1].color;
         tempColor.r = powers.item2?1f:0f;
         tempColor.g = powers.item2?1f:0f;
