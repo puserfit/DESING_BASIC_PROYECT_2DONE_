74f8420431265113fe0ad0ad335289f9920d3aa3
caaledcarre <90487078+caaledcarre@users.noreply.github.com>
2023-07-21 01:18:46 -0500

    Se agrego parte de  la UI_Stats
    Se agrego parte de la UI_Stats donde contiene las vidas de personaje, los poderes, los recolectables y el puntaje
    Todavia esta en proceso

--- Assets/Script/SceneController.cs
+++ Assets/Script/SceneController.cs
@@ -15,22 +15,29 @@ public class SceneController : MonoBehaviour  //clase:SceneController   hereda:M
 	  
 	  
 	  //se agrego mas codigo
-	  AudioSource audio = gameObject.GetComponent<AudioSource>(); //Si el objeto no tiene componente de audio esta variable será NULL
+	  
+	  //Se agrego posibilidad de esperar que el audio termine de reproducirse antes de realizar el cambio de escena.
+	  
+	  AudioSource audio = gameObject.GetComponent<AudioSource>();   //Intenta optener "AudioSource" del objeto al que esta adjunto este script
+	  
+	                                                                 //Si el objeto no tiene componente de audio esta variable será NULL
       
       if(audio == null){   // Si el objeto NO TIENE componente de AUDIO se cambia de escena inmediatamente
          SceneManager.LoadScene(_escena);
       }
       else{                // Si el objeto SI TIENE componente de AUDIO solo se cambia de escena cuando el audio haya terminado de reproducirse
-         Debug.Log("[changeScene] AudioSource:  " + audio);
-         StartCoroutine(changeSceneWithAudio(_escena, audio));
-      }
+         Debug.Log("[changeScene] AudioSource:  " + audio);             //el objeto si tiene el componente "AudioSource"
+         StartCoroutine(changeSceneWithAudio(_escena, audio));          //Entonces se llama al metodo "changeSceneWithAudio(_escena, audio)"
+      }                                                                 //para cambiar de escena de manera controlada despues que sonada el audio
       Debug.Log("CALL -> changeScene: " + _escena + " END");
    }
 
-   private IEnumerator changeSceneWithAudio(int _escena, AudioSource audio){
-      Debug.Log("[changeScene]Yield Init");
+   private IEnumerator changeSceneWithAudio(int _escena, AudioSource audio){   //Metodo:"hangeSceneWithAudio(int _escena, AudioSource audio"
+      Debug.Log("[changeScene]Yield Init");                                    //Este metodo es llamado cuando el objeto tiene un audio.
       yield return new WaitUntil(() => audio.isPlaying == false); // Se ejecuta esta línea hasta que la condición sea verdadera
-      Debug.Log("[changeScene]Yield Over");
+      Debug.Log("[changeScene]Yield Over");                                      //indica cuando comienza y cuando termina uno de esperar
+	  
+	  
 	  //fin.
 	  
 	  
