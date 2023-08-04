c099a01d7a11a85728bbb8d2b6364119e446369c
caaledcarre <90487078+caaledcarre@users.noreply.github.com>
2023-08-04 01:30:22 -0500

    Nivel Uno-Nivel Dos
    Se realizo.
    -En el codigo del personaje se coloco que el personaje tuviera 3 vidas y se van descontando vidas y sale un mensaje en la consola "game over"
    -Personaje no se voltea. Queda verticalmente . Se coloco lineas del codigo
    -Se agrego sonidos de audio para la copa transladora y para el salto del personaje

--- Assets/Script/CamaraController.cs
+++ Assets/Script/CamaraController.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class CamaraController : MonoBehaviour
 {
-    public GameObject character;
+    public GameObject character;                                             //CODIGO PROFE
+	//public GameObject fallDetector;
+	//public float limiteIzquierdo = 6.55f;
+	//public float distanciaHorizontal = 16f;    //lo que le quito a la camara
 	
     void Update()
     {
-	transform.position = new Vector3(character.transform.position.x, transform.position.y, transform.position.z);
+	transform.position = new Vector3(character.transform.position.x, transform.position.y, transform.position.z);            //CODIGO PROFE
+	
+	//Poner limite a la camara para que no me muestre espacios vacios
+	
+	//Sigue al personaje Horizontalmente con el limite izquierdo
+	//float cameraX = Mathf.Max(character.transform.position.x, limiteIzquierdo);
+	//transform.position = new Vector3(cameraX, transform.position.y, transform.position.z);
+	
+	//Ajusta la posicion del "FallDetector" para que siempre este a la misma distancia horizontal que el personaje 
+	//float fallDetectorX = character.transform.position.x + distanciaHorizontal;
+	//fallDetector.transform.position = new Vector3(fallDetectorX, fallDetector.transform.position.y, fallDetector.transform.position.z);
 		
     }
 }
--- Assets/Script/CharacterController.cs
+++ Assets/Script/CharacterController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System.Collections;                              //JUEGO CAMI
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,13 +6,14 @@ public class CharacterController : MonoBehaviour
 {
     // Valores
 	
+	private int vidas = 3;
 	
 	//float nivelPiso = -0.61f;  //Este valor representa el nivel del piso para el personaje
 	float nivelDelTecho = 7.15f;       //Este valor es el Y...comienza en el aire..Gravedad: x:0 y:9.85     nivel 2: 6.8f /   7.36f
 	float limiteR = 11.68f;    //Este valor representa el limite Derecho de la camara para el personaje
 	float limiteL = -9.07f;    //Este valor representa el limite Izquierdo de la camara para el personaje    nivel 2:-9f /  185.04f
 	float velocidad = 4f;    //Velocidad de desplazamiento del personaje  4f
-	float fuerzaSalto = 95;     //x veces la masa del personaje (2 codigo) //la fuerzaSalto es 50 su propio peso (el peso del personaje)  50-70-60-90
+	float fuerzaSalto = 110;     //x veces la masa del personaje (2 codigo) //la fuerzaSalto es 50 su propio peso (el peso del personaje)  95
 	
 	float fuerzaDesplazamiento = 300;   //Fuerza en Newton que va a tener el personaje ...es grande por que tiene rozamiento (drag)
 	
@@ -22,13 +23,24 @@ public class CharacterController : MonoBehaviour
     void Start()
     {
         //Personaje siempre inicia en la posicion (x -8.92--abajo,y -0.57---arriba)         
-		gameObject.transform.position = new Vector3(-8.97f,nivelDelTecho,0);                       //nivel2:-9f  //10.15f                     //(-9f,nivelDelTecho,0)  185.04f
+		gameObject.transform.position = new Vector3(-8.97f,nivelDelTecho,0);     //nivel2:-9f  //10.15f  CUANDO COMIENZA JUEGO  //(-9f,nivelDelTecho,0)  185.04f
 		Debug.Log("INIT");      //lo que aparece en las "" es lo que se va a decir en la consola.
+		Debug.Log("VIDAS: " + vidas);          //Numero de vidas que tiene
     }
 
     
     void Update()
     {
+		
+		//Personaje se mantenga recto
+		if(gameObject.transform.rotation.z>0.3 || gameObject.transform.rotation.z< -0.3){    //esta en radianes  0.3radianes = 17 graddos  -π y -π= -3.14 y +3.14
+			Debug.Log("ROTATION: " + gameObject.transform.rotation.z);                       //registra en la consola el valor actual de la rotacion en el ejeZ
+            gameObject.transform.rotation = Quaternion.Euler(0f, 0f, 0f);                   //restablece la rotacion del personaje en ceros grados en el eje X,Y,Z
+			                                                                                //restablece a una posicion vertical "Quaternion.Euler"
+			
+		
+		}
+		
 		if(Input.GetKey("right")&& enElPiso){
 			Debug.Log("RIGHT"); 
 			gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(fuerzaDesplazamiento,0));
@@ -59,6 +71,20 @@ public class CharacterController : MonoBehaviour
             Debug.Log("OBSTACLE COLLISION");
         }
     }
+	
+	//Vamos a colocar el objeto vacio para que lance el evento cuando el personaje cae al vacio
+	private void OnTriggerEnter2D(Collider2D collision){
+		Debug.Log("Caida del personaje");
+		
+		//Cuando el personaje cae al vacio le disminuyo una vida...........devolver al personaje al punto inicial 
+		vidas -= 1;
+		
+		Debug.Log("VIDAS: " + vidas);
+        if (vidas <=0){
+			Debug.Log("GAME OVER");                                                  //Cuando se le acaba el numero de vidas
+			vidas = 3;                                                               //quitar linea
+		}
+		gameObject.transform.position = new Vector3(-8.97f,nivelDelTecho,0);         //Personaje regresa a la misma posicion                      
 
-    
+    }
 }
