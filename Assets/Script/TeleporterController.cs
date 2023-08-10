3bddfdd5e242e5f481e8d18fef85ddd843cb0d5e
caaledcarre <90487078+caaledcarre@users.noreply.github.com>
2023-08-09 23:46:59 -0500

    Se agrego sonido al personaje y a la copa amarilla
    Se realizaron cambios en el codigo "TeleporterController" Se agrego sonido en el salto del personaje y en la copa amarilla. En los 3 niveles

--- Assets/Script/TeleporterController.cs
+++ Assets/Script/TeleporterController.cs
@@ -13,20 +13,27 @@ public class TeleporterController : MonoBehaviour
 		teleporter_SFX.Play();
 		
 		//Esperar que termine la reproduccion del sonido
-		
+		StartCoroutine(goNextLevel(teleporter_SFX.clip.length)); // Pasa el nivel con un "delay" para que alcance a sonar el audio
+        gameObject.GetComponent<Renderer>().enabled = false;      // La moneda desaparece
         
         //gameObject.SetActive(false);       //Desactiva
-        Destroy(gameObject);                //Destruye
-        
-        
-        if(SceneManager.GetActiveScene().name=="NivelUno"){
-            SceneManager.LoadScene("NivelDos");
-        }
-        else{
-            SceneManager.LoadScene("NivelUno");
-        }
-        
+        //Destroy(gameObject);                //Destruye        
     }
+	
+	    // Aquí está la lógica del paso de nivel después de la pausa suficiente para que suene el audio
+    private IEnumerator goNextLevel(float delay){
+        yield return new WaitForSeconds(delay); 
+        Destroy(gameObject);
 
+        if(SceneManager.GetActiveScene().name=="NivelUno"){
+            SceneManager.LoadScene("NivelDos");                                 //Cambiar al NivelDos si esta en NivelUno
+        }
+        else if (SceneManager.GetActiveScene().name=="NivelDos"){
+            SceneManager.LoadScene("NivelTres");                                //Cambiar al NivelTres si esta en NivelDos                                        
+        }
+		else if (SceneManager.GetActiveScene().name=="NivelTres"){
+            //SceneManager.LoadScene("NivelUno");                                 //Cambiar al NivelUno si esta en NivelTres
+        }
+    }
 
 }
